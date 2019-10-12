3068f6d27c1183c3e701480b2f4fa784b986ebda
Jean-François Rompré <jrompre@gmail.com>
2019-10-12 01:38:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,4 +55,7 @@ function placeOrder(cardNumber) {
   if (undefined === cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
+  let tot = total();
+  setCart([]);
+  return `Your total cost is $${tot}, which will be charged to the card ${cardNumber}.`
 }
