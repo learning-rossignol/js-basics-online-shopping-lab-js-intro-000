0087ebb909465102e8fce1008f82c520790d7a87
Jean-François Rompré <jrompre@gmail.com>
2019-10-12 01:23:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  }
   var report = "In your cart, you have";
   for (let i = 0; i < cart.length; i++) {
     let interpolated = (i < cart.length - 1) ? ',' : '.';
